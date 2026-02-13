2d57070581eee256fe1a1d206205ab90e40b1842
Zuolong Wu <zuolong2187@gmail.com>
2026-02-13 10:22:28 -0500

    Added User based sql queries

--- backend/sql/query.sql
+++ backend/sql/query.sql
@@ -1,8 +1,39 @@
--- name: GetUser :one
-SELECT * FROM "User"
-WHERE UUID = $1 LIMIT 1;
+-- name: GetPublicUser :one
+SELECT * FROM "PublicUser"
+WHERE uuid = $1 LIMIT 1;
 
--- name: GetArtists :many
+-- name: GetHashPassword :one
+SELECT hashed_password FROM "User"
+WHERE uuid = $1
+
+-- name: UpdatePassword :exec
+UPDATE "User"
+SET hashed_password = $2
+WHERE uuid = $1;
+
+-- name: UpdateProfile :exec
+UPDATE "User"
+SET username = $2,
+    bio = $3,
+    updated = CURRENT_TIMESTAMP
+WHERE uuid = $1;
+
+-- name: UpdateEmail :exec
+UPDATE "User"
+SET email = $2
+WHERE uuid = $1
+
+-- name: UpdateImage :exec
+UPDATE "User"
+SET profile_image_path = $2
+WHERE uuid = %1
+
+
+-- name: GetArtist :one
+SELECT * FROM "Artist"
+WHERE uuid = $1
+
+-- name: GetArtistsAlphabetically :many
 SELECT * FROM "Artist"
 ORDER BY artist_name;
 
@@ -10,9 +41,6 @@ ORDER BY artist_name;
 SELECT * FROM "Album"
 ORDER BY created_at DESC;
 
--- name: UpdatePassword :exec
-UPDATE "User"
-set hashed_password = $2
-WHERE UUID = $1;
+
 
 
