429da93ca27156fae7bd831c49f2133f4e4f5c67
Zuolong Wu <zuolong2187@gmail.com>
2026-02-13 11:59:59 -0500

    Added hashing to backend

---
+++ backend/internal/helpers/hashing.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"github.com/matthewhartstonge/argon2"
+)
+
+func encode(password string) string {
+	argon := argon2.DefaultConfig()
+
+	encoded, err := argon.HashEncoded([]byte(password))
+	if err != nil {
+		panic(err)
+	}
+
+	return string(encoded)
+}
+
+func verify(password string, encodedPassword string) bool {
+	ok, err := argon2.VerifyEncoded([]byte(password), []byte(encodedPassword))
+	if err != nil {
+		panic(err)
+	}
+
+	return ok
+}
