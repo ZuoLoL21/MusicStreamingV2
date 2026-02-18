326243a9faa36c468f6fc85ff60b8cad97b213ee
Zuolong Wu <zuolong2187@gmail.com>
2026-02-18 10:32:58 -0500

    Fixed db to correctly show the signatures

--- user_database/internal/handlers/db.go
+++ user_database/internal/handlers/db.go
@@ -21,7 +21,7 @@ type DB interface {
 	UpdateImage(ctx context.Context, arg sqlhandler.UpdateImageParams) error
 	GetArtistForUser(ctx context.Context, userUuid pgtype.UUID) ([]sqlhandler.GetArtistForUserRow, error)
 
-	GetArtistsAlphabetically(ctx context.Context) ([]sqlhandler.Artist, error)
+	GetArtistsAlphabetically(ctx context.Context, arg sqlhandler.GetArtistsAlphabeticallyParams) ([]sqlhandler.Artist, error)
 	GetArtist(ctx context.Context, uuid pgtype.UUID) (sqlhandler.Artist, error)
 	CreateArtist(ctx context.Context, arg sqlhandler.CreateArtistParams) error
 	UpdateArtistProfile(ctx context.Context, arg sqlhandler.UpdateArtistProfileParams) error
@@ -32,16 +32,16 @@ type DB interface {
 	ChangeUserRole(ctx context.Context, arg sqlhandler.ChangeUserRoleParams) error
 
 	GetAlbum(ctx context.Context, uuid pgtype.UUID) (sqlhandler.Album, error)
-	GetAlbumsForArtist(ctx context.Context, fromArtist pgtype.UUID) ([]sqlhandler.Album, error)
+	GetAlbumsForArtist(ctx context.Context, arg sqlhandler.GetAlbumsForArtistParams) ([]sqlhandler.Album, error)
 	CreateAlbum(ctx context.Context, arg sqlhandler.CreateAlbumParams) error
 	UpdateAlbum(ctx context.Context, arg sqlhandler.UpdateAlbumParams) error
 	UpdateAlbumImage(ctx context.Context, arg sqlhandler.UpdateAlbumImageParams) error
 	DeleteAlbum(ctx context.Context, uuid pgtype.UUID) error
 
 	GetMusic(ctx context.Context, uuid pgtype.UUID) (sqlhandler.Music, error)
-	GetMusicForArtist(ctx context.Context, fromArtist pgtype.UUID) ([]sqlhandler.Music, error)
-	GetMusicForAlbum(ctx context.Context, inAlbum pgtype.UUID) ([]sqlhandler.Music, error)
-	GetMusicForUser(ctx context.Context, uploadedBy pgtype.UUID) ([]sqlhandler.Music, error)
+	GetMusicForArtist(ctx context.Context, arg sqlhandler.GetMusicForArtistParams) ([]sqlhandler.Music, error)
+	GetMusicForAlbum(ctx context.Context, arg sqlhandler.GetMusicForAlbumParams) ([]sqlhandler.Music, error)
+	GetMusicForUser(ctx context.Context, arg sqlhandler.GetMusicForUserParams) ([]sqlhandler.Music, error)
 	CreateMusic(ctx context.Context, arg sqlhandler.CreateMusicParams) error
 	UpdateMusicDetails(ctx context.Context, arg sqlhandler.UpdateMusicDetailsParams) error
 	UpdateMusicStorage(ctx context.Context, arg sqlhandler.UpdateMusicStorageParams) error
@@ -49,32 +49,30 @@ type DB interface {
 	IncrementPlayCount(ctx context.Context, uuid pgtype.UUID) error
 	AddListeningHistoryEntry(ctx context.Context, arg sqlhandler.AddListeningHistoryEntryParams) error
 
-	GetLikesForMusic(ctx context.Context, toMusic pgtype.UUID) ([]sqlhandler.PublicUser, error)
-	GetLikesForUser(ctx context.Context, fromUser pgtype.UUID) ([]sqlhandler.Music, error)
+	GetLikesForUser(ctx context.Context, arg sqlhandler.GetLikesForUserParams) ([]sqlhandler.Music, error)
 	IsLiked(ctx context.Context, arg sqlhandler.IsLikedParams) (bool, error)
 	LikeMusic(ctx context.Context, arg sqlhandler.LikeMusicParams) error
 	UnlikeMusic(ctx context.Context, arg sqlhandler.UnlikeMusicParams) error
 
-	GetFollowersForUser(ctx context.Context, toUser pgtype.UUID) ([]sqlhandler.PublicUser, error)
-	GetFollowingUsersForUser(ctx context.Context, fromUser pgtype.UUID) ([]sqlhandler.PublicUser, error)
-	GetFollowingArtistsForUser(ctx context.Context, fromUser pgtype.UUID) ([]sqlhandler.Artist, error)
-	GetFollowersForArtist(ctx context.Context, toArtist pgtype.UUID) ([]sqlhandler.PublicUser, error)
+	GetFollowersForUser(ctx context.Context, arg sqlhandler.GetFollowersForUserParams) ([]sqlhandler.PublicUser, error)
+	GetFollowsForUser(ctx context.Context, arg sqlhandler.GetFollowsForUserParams) ([]sqlhandler.PublicUser, error)
+	GetFollowersForArtist(ctx context.Context, arg sqlhandler.GetFollowersForArtistParams) ([]sqlhandler.PublicUser, error)
 	FollowUser(ctx context.Context, arg sqlhandler.FollowUserParams) error
 	UnfollowUser(ctx context.Context, arg sqlhandler.UnfollowUserParams) error
 	FollowArtist(ctx context.Context, arg sqlhandler.FollowArtistParams) error
 	UnfollowArtist(ctx context.Context, arg sqlhandler.UnfollowArtistParams) error
 
-	GetAllTags(ctx context.Context) ([]sqlhandler.MusicTag, error)
+	GetAllTags(ctx context.Context, arg sqlhandler.GetAllTagsParams) ([]sqlhandler.MusicTag, error)
 	GetTag(ctx context.Context, tagName string) (sqlhandler.MusicTag, error)
-	GetMusicForTag(ctx context.Context, tagName string) ([]sqlhandler.Music, error)
-	GetTagsForMusic(ctx context.Context, musicUuid pgtype.UUID) ([]sqlhandler.MusicTag, error)
+	GetMusicForTag(ctx context.Context, arg sqlhandler.GetMusicForTagParams) ([]sqlhandler.Music, error)
+	GetTagsForMusic(ctx context.Context, arg sqlhandler.GetTagsForMusicParams) ([]sqlhandler.MusicTag, error)
 	CreateTag(ctx context.Context, arg sqlhandler.CreateTagParams) error
 	AssignTagToMusic(ctx context.Context, arg sqlhandler.AssignTagToMusicParams) error
 	RemoveTagFromMusic(ctx context.Context, arg sqlhandler.RemoveTagFromMusicParams) error
 
 	GetPlaylist(ctx context.Context, uuid pgtype.UUID) (sqlhandler.Playlist, error)
-	GetPlaylistsForUser(ctx context.Context, fromUser pgtype.UUID) ([]sqlhandler.Playlist, error)
-	GetPlaylistTracks(ctx context.Context, playlistUuid pgtype.UUID) ([]sqlhandler.Music, error)
+	GetPlaylistsForUser(ctx context.Context, arg sqlhandler.GetPlaylistsForUserParams) ([]sqlhandler.Playlist, error)
+	GetPlaylistTracks(ctx context.Context, arg sqlhandler.GetPlaylistTracksParams) ([]sqlhandler.Music, error)
 	CreatePlaylist(ctx context.Context, arg sqlhandler.CreatePlaylistParams) error
 	UpdatePlaylist(ctx context.Context, arg sqlhandler.UpdatePlaylistParams) error
 	DeletePlaylist(ctx context.Context, uuid pgtype.UUID) error
@@ -82,7 +80,7 @@ type DB interface {
 	RemoveTrackFromPlaylist(ctx context.Context, arg sqlhandler.RemoveTrackFromPlaylistParams) error
 	UpdateTrackPosition(ctx context.Context, arg sqlhandler.UpdateTrackPositionParams) error
 
-	GetListeningHistoryForUser(ctx context.Context, userUuid pgtype.UUID) ([]sqlhandler.ListeningHistory, error)
+	GetListeningHistoryForUser(ctx context.Context, arg sqlhandler.GetListeningHistoryForUserParams) ([]sqlhandler.ListeningHistory, error)
 	GetRecentlyPlayedForUser(ctx context.Context, arg sqlhandler.GetRecentlyPlayedForUserParams) ([]sqlhandler.ListeningHistory, error)
 	GetTopMusicForUser(ctx context.Context, arg sqlhandler.GetTopMusicForUserParams) ([]sqlhandler.GetTopMusicForUserRow, error)
 }
