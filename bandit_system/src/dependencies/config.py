97c89dd827912d8772cbd46fbf0e13bb6eaba5d6
Zuolong Wu <zuolong2187@gmail.com>
2026-02-19 15:59:44 -0500

    Updated config and .env to be representative of the table names

--- bandit_system/src/dependencies/config.py
+++ bandit_system/src/dependencies/config.py
@@ -8,7 +8,6 @@ class Config:
 
         self.db_warehouse_string = os.getenv("DB_CONNECTION_STRING_WAREHOUSE")
         self.bandit_data_table = os.getenv("WAREHOUSE_BANDIT_DATA_TABLE")
-        self.music_theme_table = os.getenv("WAREHOUSE_MUSIC_THEME_TABLE")
 
         self.db_params_string = os.getenv("DB_CONNECTION_STRING_PARAMS")
         self.bandit_params_string = os.getenv("BANDIT_DATA_TABLE")
