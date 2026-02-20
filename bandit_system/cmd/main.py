f24ab3d111e3816f62c97e09f224be35f1ebb947
Zuolong Wu <zuolong2187@gmail.com>
2026-02-20 14:00:02 -0500

    Added a sample fastapi

--- bandit_system/cmd/main.py
+++ bandit_system/cmd/main.py
@@ -1,24 +1,7 @@
-import numpy as np
+from fastapi import FastAPI
 
-from src.models.thompson import ArmResult, predict
+app = FastAPI()
 
-actual_prob = [0.1, 0.7, 0.5]
-
-data = [
-    ArmResult(Success=0, Failures=0, ArmName="1"),
-    ArmResult(Success=0, Failures=0, ArmName="2"),
-    ArmResult(Success=0, Failures=0, ArmName="3"),
-]
-
-
-for trial in range(101):
-    predicted = predict(data)
-
-    if np.random.uniform() < actual_prob[predicted]:
-        data[predicted].Success += 1
-    else:
-        data[predicted].Failures += 1
-
-    # logging
-    if trial % 10 == 0:
-        print(f"trial: {trial}\tdata: {data}")
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
---
+++ bandit_system/src/middleware/auth.py
@@ -0,0 +1,1 @@
+# TODO: add an auth middleware
