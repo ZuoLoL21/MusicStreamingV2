939600c0b71034a673de121f4a4a5cf3aea9c82c
Zuolong Wu <zuolong2187@gmail.com>
2026-02-23 10:11:44 -0500

    Added comprehensive logger

--- bandit_system/cmd/main.py
+++ bandit_system/cmd/main.py
@@ -1,16 +1,32 @@
 import logging
+
+import structlog
 from fastapi import FastAPI
 from src.middleware import RequestIDMiddleware, LoggingMiddleware
 
-# Configure logging
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-logger = logging.getLogger(__name__)
+structlog.configure(
+    processors=[
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.JSONRenderer(),
+    ],
+    wrapper_class=structlog.BoundLogger,
+    context_class=dict,
+    logger_factory=structlog.PrintLoggerFactory(),
+    cache_logger_on_first_use=True,
+)
 
+logger = structlog.get_logger("bandit_system")
+logger.info("Bandit system starting up")
 app = FastAPI()
 
-# Add middlewares (order matters - request_id should be first)
+# Middleware
 app.add_middleware(RequestIDMiddleware)
 app.add_middleware(LoggingMiddleware, logger=logger)
 
