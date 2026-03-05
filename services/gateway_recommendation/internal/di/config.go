2bfa638eeebad8d417220272dffe6515c31184ab
Zuolong Wu <zuolong2187@gmail.com>
2026-03-05 15:45:14 -0500

    Changed name of environment variable

--- services/gateway_recommendation/internal/di/config.go
+++ services/gateway_recommendation/internal/di/config.go
@@ -36,7 +36,7 @@ func LoadConfig(logger *zap.Logger) *Config {
 	}
 
 	// Load environment variables
-	port := os.Getenv("GATEWAY_API_PORT")
+	port := os.Getenv("GATEWAY_RECOMMENDATION_PORT")
 	popularityServiceURL := os.Getenv("POPULARITY_SERVICE_URL")
 	banditServiceURL := os.Getenv("BANDIT_SERVICE_URL")
 	jwtStorePath := os.Getenv("JWT_STORE_PATH")
