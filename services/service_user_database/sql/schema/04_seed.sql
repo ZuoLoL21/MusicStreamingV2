17b539a20411172e45321e2fa71ec8585dbe8a95
Zuolong Wu <zuolong2187@gmail.com>
2026-03-05 15:39:11 -0500

    Fixed seeding not working

--- services/service_user_database/sql/schema/04_seed.sql
+++ services/service_user_database/sql/schema/04_seed.sql
@@ -1,7 +1,7 @@
 
 -- Insert Sample Users
-INSERT INTO users (uuid, username, email, hashed_password, bio) VALUES
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'default', 'john@example.com', '$2a$10$hashedpassword1', 'Very real human man');
+INSERT INTO users (uuid, username, email, hashed_password, bio, country) VALUES
+    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'default', 'john@example.com', '$2a$10$hashedpassword1', 'Very real human man', 'US');
 
 -- Insert Genre Tags
 INSERT INTO music_tags (tag_name, tag_description) VALUES
